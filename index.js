5936c01b63cac59b60030a270423a795c216d650
Opirajbhor <opiraj6@gmail.com>
2025-12-20 20:19:58 +0600

    vercel fixed now v02

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ import dotenv from "dotenv";
 import { client } from "./mongoClient.js";
 import { initializeApp } from "firebase-admin/app";
 import { ObjectId } from "mongodb";
-import Stripe from 'stripe';
+import Stripe from "stripe";
 import admin from "firebase-admin";
-import serviceAccount from "./AmarShohor-firebaseAdminSDK.json" with { type: 'json' };
+import serverless from "serverless-http";
+
+dotenv.config();
+
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
+  "utf8"
+);
+const serviceAccount = JSON.parse(decoded);
 
 // firebase admin sdk
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
 const stripe = new Stripe(process.env.STRIPE_KEY);
@@ -49,16 +55,16 @@ async function connectDB() {
     const all_Issues = db.collection("All_issues");
     const timeline = db.collection("timeline");
     const users = db.collection("users");
-    const paymentCollection = db.collection("payments")
+    const paymentCollection = db.collection("payments");
 
     // GET data starts---------------------------
-      // user role---------
-      app.get('/user/:email/role', async (req, res)=>{
-       const email = req.params.email
-       const query = {email}
-       const result = await users.findOne(query)
-       res.send(result?.role || 'Citizen')
-      })
+    // user role---------
+    app.get("/user/:email/role", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const result = await users.findOne(query);
+      res.send(result?.role || "Citizen");
+    });
 
     // all issues get
     app.get("/all-issues", async (req, res) => {
@@ -70,24 +76,28 @@ async function connectDB() {
       }
     });
     // get latest issuess-------------
-    app.get('/latest-issues', async(req, res)=>{
+    app.get("/latest-issues", async (req, res) => {
       try {
-        const result = await all_Issues.find().sort({ _id: -1 }).limit(6).toArray();
+        const result = await all_Issues
+          .find()
+          .sort({ _id: -1 })
+          .limit(6)
+          .toArray();
         res.status(200).json(result);
       } catch (err) {
         res.status(500).json({ mesage: "data load failled" });
       }
-    })
+    });
 
     // get user issue Data-------------
-    app.get("/user-issues",verifyJWT, async (req, res) => {
-      const userEmail = req.headers.email
+    app.get("/user-issues", verifyJWT, async (req, res) => {
+      const userEmail = req.headers.email;
       if (!userEmail) {
         return res.status(400).json({ message: "user is missing." });
-          }
-          try {
-        const query = {reportedBy:userEmail}
-              // Debug here:
+      }
+      try {
+        const query = { reportedBy: userEmail };
+        // Debug here:
         const issues = await all_Issues.find(query).toArray();
         res.status(200).json(issues);
       } catch (err) {
@@ -96,33 +106,31 @@ async function connectDB() {
     });
 
     // get single issue data
-    app.get('/all-issues/:id', async(req, res)=>{
-    const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await all_Issues.findOne(query)
-      res.send(result)
-    })
-
+    app.get("/all-issues/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await all_Issues.findOne(query);
+      res.send(result);
+    });
 
     // get user payments
-    app.get('/user-payments',verifyJWT, async(req,res)=>{
-      const userEmail = req.headers.email
-        if (!userEmail) {
+    app.get("/user-payments", verifyJWT, async (req, res) => {
+      const userEmail = req.headers.email;
+      if (!userEmail) {
         return res.status(400).json({ message: "user is missing." });
-          }
-          try {
-        const query = {email:userEmail}
+      }
+      try {
+        const query = { email: userEmail };
         const issues = await paymentCollection.find(query).toArray();
         res.status(200).json(issues);
       } catch (err) {
         res.status(500).send({ message: "Server Error", error: err.message });
       }
-    })
-
+    });
 
     // Admin----------------
     // manage issues------------
-       app.get("/manage-issues",verifyJWT, async (req, res) => {
+    app.get("/manage-issues", verifyJWT, async (req, res) => {
       try {
         const result = await all_Issues.find().toArray();
         res.status(200).json(result);
@@ -131,34 +139,34 @@ async function connectDB() {
       }
     });
     // manage users----------------
-       app.get("/manage-users",verifyJWT, async (req, res) => {
+    app.get("/manage-users", verifyJWT, async (req, res) => {
       try {
-        const result = await users.find({role:"Citizen"}).toArray();
+        const result = await users.find({ role: "Citizen" }).toArray();
         res.status(200).json(result);
       } catch (err) {
         res.status(500).json({ mesage: "data load failled" });
       }
     });
     // manage staff----------------
-       app.get("/manage-staff",verifyJWT, async (req, res) => {
+    app.get("/manage-staff", verifyJWT, async (req, res) => {
       try {
-        const result = await users.find({role:"Staff"}).toArray();
+        const result = await users.find({ role: "Staff" }).toArray();
         res.status(200).json(result);
       } catch (err) {
         res.status(500).json({ mesage: "data load failled" });
       }
     });
     // staff list----------------
-       app.get("/staff-list",verifyJWT, async (req, res) => {
+    app.get("/staff-list", verifyJWT, async (req, res) => {
       try {
-        const result = await users.find({role: 'Staff'}).toArray();
+        const result = await users.find({ role: "Staff" }).toArray();
         res.status(200).json(result);
       } catch (err) {
         res.status(500).json({ mesage: "data load failled" });
       }
     });
     // manage payments----------------
-       app.get("/manage-payments",verifyJWT, async (req, res) => {
+    app.get("/manage-payments", verifyJWT, async (req, res) => {
       try {
         const result = await paymentCollection.find().toArray();
         res.status(200).json(result);
@@ -168,25 +176,26 @@ async function connectDB() {
     });
 
     // staff issues----------------
-    app.get("/assign-issues",verifyJWT, async (req, res)=>{
-          const {name} = req.query
-           if (!name) {
-             return res.status(400).json({ message: "user is missing." });
-            }
-            try{
-              const result = await all_Issues.find({assignedTo : "Opi Rajbhor"}).toArray()
-              res.status(200).json(result);
-            }
-       catch (err) {
+    app.get("/assign-issues", verifyJWT, async (req, res) => {
+      const { name } = req.query;
+      if (!name) {
+        return res.status(400).json({ message: "user is missing." });
+      }
+      try {
+        const result = await all_Issues
+          .find({ assignedTo: "Opi Rajbhor" })
+          .toArray();
+        res.status(200).json(result);
+      } catch (err) {
         res.status(500).json({ mesage: "data load failled" });
-      
-  }})
+      }
+    });
     // Get data ends--------------------------------------
 
     // ***********Post Data starts-------------------------------------
 
     // post an issue
-    app.post("/all-issues",verifyJWT, async (req, res) => {
+    app.post("/all-issues", verifyJWT, async (req, res) => {
       const { title, description, email, location, category, image } = req.body;
 
       const userLimit = all_Issues.countDocuments({ reportedBy: email });
@@ -208,7 +217,7 @@ async function connectDB() {
           createdAt: new Date(),
           upvoteCount: 0,
           assignedTo: "Not Assigned Yet",
-          isBoosted:false,
+          isBoosted: false,
         };
         const result = await all_Issues.insertOne(newIssue);
 
@@ -260,9 +269,9 @@ async function connectDB() {
     });
     // staff create-----------------
     // post user info from registration form method
-    app.post("/staff",verifyJWT, async (req, res) => {
+    app.post("/staff", verifyJWT, async (req, res) => {
       try {
-        const { name, imageURL, email,phone } = req.body;
+        const { name, imageURL, email, phone } = req.body;
         const isUserAvailable = await users.countDocuments({ email: email });
         if (isUserAvailable) {
           return res.send("staff data already available in db");
@@ -307,177 +316,169 @@ async function connectDB() {
         res.send("data send failled to db", result);
       }
     });
-// payment api for boost issue------------
-app.post('/create-checkout-session',verifyJWT, async (req, res) => {
-    const paymentInfo =  req.body
-    const session = await stripe.checkout.sessions.create({
-         line_items: [
-      {
-        price_data: {
-          currency : 'BDT',
-          unit_amount: 100000,
-          product_data: {
-            name: paymentInfo.title,
+    // payment api for boost issue------------
+    app.post("/create-checkout-session", verifyJWT, async (req, res) => {
+      const paymentInfo = req.body;
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: "BDT",
+              unit_amount: 100000,
+              product_data: {
+                name: paymentInfo.title,
+              },
+            },
+            quantity: 1,
           },
+        ],
+        customer_email: paymentInfo.email,
+        mode: "payment",
+        metadata: {
+          issueId: paymentInfo.issueId,
+          issueTitle: paymentInfo.title,
         },
-        quantity: 1,
-      },
-    ],
-    customer_email: paymentInfo.email,
-    mode: 'payment',
-    metadata: {
-      issueId:paymentInfo.issueId,
-      issueTitle: paymentInfo.title,
-    },
-    success_url: `${process.env.CLIENT_URL}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `${process.env.CLIENT_URL}/dashboard/payment-cancel`,
-    })
-    res.json({url: session.url})
-    })
-    // Post Data ends---------------------------------------
-    
-    // patch Data Starts-------------------------
-    app.patch("/all-issues/:id",verifyJWT, async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updates = req.body;
-
-    const result = await all_Issues.updateOne(
-      { _id: new ObjectId(id) },
-      { $set: updates }
-    );
-
-    if (result.matchedCount === 0) {
-      return res.status(404).send({ message: "Issue not found" });
-    }
-
-    res.send({ success: true });
-  } catch (error) {
-    res.status(400).send({ message: "Update failed" });
-  }
-});
-    // patch Data Starts-------------------------
-    app.patch("/manage-users/:id",verifyJWT, async (req, res) => {
-  try {
-    const { id } = req.params;
-    const blockState = req.body;
-
-    const result = await users.updateOne(
-      { _id: new ObjectId(id) },
-      { $set: blockState }
-    );
-
-    if (result.matchedCount === 0) {
-      return res.status(404).send({ message: "Issue not found" });
-    }
-
-    res.send({ success: true });
-  } catch (error) {
-    res.status(400).send({ message: "Update failed" });
-  }
-});
-
-// update profile--------
-app.patch('/profile-update',verifyJWT, async(req, res)=>{
-  const { name, photoURL } = req.body;
-  const email = req.tokenEmail
-  const update = {
-    $set:{name, photoURL}
-  }
-  const result = await users.updateOne({email}, update)
-  if (result.matchedCount === 0) {
-      return res.status(404).send({ message: "User not found" });
-    }
-
-    res.send({
-      success: true,
-      message: "Profile updated successfully",
+        success_url: `${process.env.CLIENT_URL}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.CLIENT_URL}/dashboard/payment-cancel`,
+      });
+      res.json({ url: session.url });
     });
-})
+    // Post Data ends---------------------------------------
 
+    // patch Data Starts-------------------------
+    app.patch("/all-issues/:id", verifyJWT, async (req, res) => {
+      try {
+        const { id } = req.params;
+        const updates = req.body;
 
-// payment data update
-app.patch('/payment-success',verifyJWT, async (req,res)=>{
-  const sessionId = req.query.session_id
-  const session = await stripe.checkout.sessions.retrieve(sessionId)
- const transactionId = session.payment_intent
- const query = {transactionId : transactionId}
- const isPaymentExist = await paymentCollection.findOne(query)
-  if(isPaymentExist){
-    return res.send({message: "already exist", transactionId: transactionId})
-  }
+        const result = await all_Issues.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: updates }
+        );
 
+        if (result.matchedCount === 0) {
+          return res.status(404).send({ message: "Issue not found" });
+        }
 
-if(session?.payment_status === 'paid'){
-  const paidIssueId = session?.metadata.issueId
-  const query = {_id: new ObjectId(paidIssueId)}
-  const update = {
-    $set : {
-      isBoosted : true,
-    },
-  }
-  const result = await all_Issues.updateOne(query, update)
+        res.send({ success: true });
+      } catch (error) {
+        res.status(400).send({ message: "Update failed" });
+      }
+    });
+    // patch Data Starts-------------------------
+    app.patch("/manage-users/:id", verifyJWT, async (req, res) => {
+      try {
+        const { id } = req.params;
+        const blockState = req.body;
 
-  const payment ={
-    amount : 1000,
-    currency : session.currency,
-    email : session.customer_email,
-    title : session.metadata.issueTitle,
-    id : session.metadata.issueId,
-    transactionId : session.payment_intent,
-    paymentStatus : session.payment_status,
-    paidAt : new Date (),
-  }
+        const result = await users.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: blockState }
+        );
 
+        if (result.matchedCount === 0) {
+          return res.status(404).send({ message: "Issue not found" });
+        }
 
-  if(session?.payment_status === 'paid'){
-    const resultPayment = await paymentCollection.insertOne(payment)
-    res.send({
-      success:true, 
-      modifyIssue : result, 
-      paymentInfo: resultPayment,
-      transactionId : session.payment_intent})
-  }
+        res.send({ success: true });
+      } catch (error) {
+        res.status(400).send({ message: "Update failed" });
+      }
+    });
 
-}
-  res.send({success: false})
-})
+    // update profile--------
+    app.patch("/profile-update", verifyJWT, async (req, res) => {
+      const { name, photoURL } = req.body;
+      const email = req.tokenEmail;
+      const update = {
+        $set: { name, photoURL },
+      };
+      const result = await users.updateOne({ email }, update);
+      if (result.matchedCount === 0) {
+        return res.status(404).send({ message: "User not found" });
+      }
+
+      res.send({
+        success: true,
+        message: "Profile updated successfully",
+      });
+    });
+
+    // payment data update
+    app.patch("/payment-success", verifyJWT, async (req, res) => {
+      const sessionId = req.query.session_id;
+      const session = await stripe.checkout.sessions.retrieve(sessionId);
+      const transactionId = session.payment_intent;
+      const query = { transactionId: transactionId };
+      const isPaymentExist = await paymentCollection.findOne(query);
+      if (isPaymentExist) {
+        return res.send({
+          message: "already exist",
+          transactionId: transactionId,
+        });
+      }
+
+      if (session?.payment_status === "paid") {
+        const paidIssueId = session?.metadata.issueId;
+        const query = { _id: new ObjectId(paidIssueId) };
+        const update = {
+          $set: {
+            isBoosted: true,
+          },
+        };
+        const result = await all_Issues.updateOne(query, update);
+
+        const payment = {
+          amount: 1000,
+          currency: session.currency,
+          email: session.customer_email,
+          title: session.metadata.issueTitle,
+          id: session.metadata.issueId,
+          transactionId: session.payment_intent,
+          paymentStatus: session.payment_status,
+          paidAt: new Date(),
+        };
+
+        if (session?.payment_status === "paid") {
+          const resultPayment = await paymentCollection.insertOne(payment);
+          res.send({
+            success: true,
+            modifyIssue: result,
+            paymentInfo: resultPayment,
+            transactionId: session.payment_intent,
+          });
+        }
+      }
+      res.send({ success: false });
+    });
     // patch Data ends-------------------------
 
     // Delete Data Starts------------------
-  app.delete('/all-issues/:id',verifyJWT, async (req, res)=>{
-    const issueId = req.params.id
-    const doc = await all_Issues.findOne();
-    const query = {_id: new ObjectId(issueId)}
-    const result =   await all_Issues.deleteOne(query)
-    res.status(200).send({message: "Issue deleted"})
-  })
-  app.delete('/staff-delete/:id',verifyJWT, async (req, res)=>{
-    const staffId = req.params.id
-    const query = await users.findOne({_id :new ObjectId(staffId) });
-    const result =   await users.deleteOne(query)
-    res.status(200).send({message: "Staff deleted"})
-  })
+    app.delete("/all-issues/:id", verifyJWT, async (req, res) => {
+      const issueId = req.params.id;
+      const doc = await all_Issues.findOne();
+      const query = { _id: new ObjectId(issueId) };
+      const result = await all_Issues.deleteOne(query);
+      res.status(200).send({ message: "Issue deleted" });
+    });
+    app.delete("/staff-delete/:id", verifyJWT, async (req, res) => {
+      const staffId = req.params.id;
+      const query = await users.findOne({ _id: new ObjectId(staffId) });
+      const result = await users.deleteOne(query);
+      res.status(200).send({ message: "Staff deleted" });
+    });
 
     // Delete Data ends------------------
-
   } catch (error) {
     process.exit(1); // stop server if DB connection fails
-  }
-  finally {
+  } finally {
     app.listen(port, () => {
       console.log(`Example app listening on port ${port}`);
-    })}
+    });
+  }
 }
 connectDB().catch(console.dir);
 // Routes
 app.get("/", (req, res) => {
   res.send("Backend is Running!");
 });
-
-// Start server only after DB connects
-// connectDB().then(() => {
-//   app.listen(port, () => {
-//     console.log("Server running on port", port);
-//   });
-// });
